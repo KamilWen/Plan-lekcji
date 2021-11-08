3e12a8954d25912ce6ff287fbbe1b9bd76e190a5
KamilWen <91737917+KamilWen@users.noreply.github.com>
2021-11-08 13:39:41 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PLAN LEKCJI</title>
+    <style>
+        table {
+            background-image: linear-gradient(
+                #09dbca, #1ad672
+            );
+        }
+        table, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+
+        tr, td {
+            margin: 0;
+            padding: 10px 15px;
+            text-align: center;
+        }
+
+        .header {
+            background-color: rgb(155, 155, 155);
+        }
+    </style>
+</head>
+<body>
+    <table>
+        <th colspan='6' class='header'>PLAN LEKCJI</th>
+        <tr class='header'>
+            <td>Godzina</td>
+            <td>Poniedziałek</td>
+            <td>Wtorek</td>
+            <td>Sroda</td>
+            <td>Czwartek</td>
+            <td>Piątek</td>
+        </tr>
+        <?php
+            function console_log( $data ){
+                echo '<script>';
+                echo 'console.log('. json_encode( $data ) .')';
+                echo '</script>';
+            }
+            $baza = mysqli_connect('localhost', 'root', '', 'plan');
+            if(mysqli_errno($baza)) {
+                echo "Error";
+            }
+
+            for ($h=1; $h <= 9; $h++) {
+                echo "<tr><td class='header'>$h</td>";
+                for ($d=1; $d <= 5; $d++) {
+                    $result = mysqli_query($baza, "SELECT * FROM plan WHERE hour=$h AND day=$d;");
+                    $row = mysqli_fetch_array($result);
+                    if($row) {
+                        echo "<td>$row[3]</td>";
+                    } else echo "<td></td>";
+                }
+                echo "</tr>";
+            }
+        ?>
+    </table>
+</body>
+</html>
